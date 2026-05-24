df959a4180af7885c5ba4254136497fd796bc4b9
Macutie <macelroybadar@gmail.com>
2026-05-24 23:56:36 +0800

    Improved Commit

---
+++ examples/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello from uploaded C file\n");
+    return 0;
+}
---
+++ examples/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from uploaded C++ file" << std::endl;
+    return 0;
+}
